a58b274d955164f0f4704c98581f6daa59d5ae4e
Dmitry Ovsyanko <do_@pochta.ru>
2017-07-11 09:53:38 +0300

    fill: data-name

--- elu.js
+++ elu.js
@@ -439,6 +439,7 @@ function fill (jq, data, target) {
     
     eachAttr (jq, 'data-text',   data, function (me, n, v) {me.text (v)})
     eachAttr (jq, 'data-id-field', data, function (me, n, v) {me.attr ('data-id', v)})
+    eachAttr (jq, 'data-name',   data, function (me, n, v) {me.attr ('name', v)})
     eachAttr (jq, 'data-for',    data, function (me, n, v) {me.attr ('for', v)})
     eachAttr (jq, 'data-value',  data, function (me, n, v) {me.val (v)})
     eachAttr (jq, 'data-class',  data, function (me, n, v) {me.addClass (v)})
