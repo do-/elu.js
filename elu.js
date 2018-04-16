696929728344cafa68ad0beb32ee63e2f70296f8
Dmitry Ovsyanko <do_@pochta.ru>
2018-04-16 17:13:47 +0300

    fill: data-html attr

--- elu.js
+++ elu.js
@@ -568,6 +568,7 @@ function fill (jq, data, target) {
         
     })
     
+    eachAttr (jq, 'data-html',   data, function (me, n, v) {me.html (v)})
     eachAttr (jq, 'data-id-field', data, function (me, n, v) {me.attr ('data-id', v)})
     eachAttr (jq, 'data-name',   data, function (me, n, v) {me.attr ('name', v)})
     eachAttr (jq, 'data-for',    data, function (me, n, v) {me.attr ('for', v)})
