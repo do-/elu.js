do-/elu.js

commit 6ee7f9edfb09f12294d4a9344d88b27bbfae8615

refill

elu.js
@@ -480,6 +480,10 @@ function query (tia, data, done, fail) {
 
 }
 
+function refill (data, target) {
+    fill (target, data, target)
+}
+
 function fill (jq, data, target) {
 
     jq = jq.clone ()
@@ -491,7 +495,7 @@ function fill (jq, data, target) {
             var name = me.attr (a)
             var names = name.split ('.')
             if (names.length == 1) {
-                todo (me.removeAttr (a), name, data [name])
+                todo (me, name, data [name])
             }
             else {
                 var d = data
@@ -501,7 +505,7 @@ function fill (jq, data, target) {
                     d = ''
                     break
                 }
-                todo (me.removeAttr (a), name, d)
+                todo (me, name, d)
             }
         })
 
@@ -578,8 +582,8 @@ function fill (jq, data, target) {
     eachAttr (jq, 'data-value',  data, function (me, n, v) {me.val (v)})
     eachAttr (jq, 'data-class',  data, function (me, n, v) {me.addClass (v)})
     eachAttr (jq, 'data-key',    data, function (me, n, v) {me.text (me.text () + ' (' + n + ')'); me.attr ('data-hotkey', n)})
-    eachAttr (jq, 'data-off',    data, function (me, n, v) {if (v) me.remove ()})
-    eachAttr (jq, 'data-on',     data, function (me, n, v) {if (!v) me.remove ()})
+    eachAttr (jq, 'data-off',    data, function (me, n, v) {if ( v) me.hide (); else me.show ()})
+    eachAttr (jq, 'data-on',     data, function (me, n, v) {if (!v) me.hide (); else me.show ()})
     eachAttr (jq, 'data-roles',  data, function (me, n, v) {if (n.split (/\W/).indexOf ($_USER.role) < 0) me.remove ()})
     eachAttr (jq, 'data-uri',    data, function (me, n, v) {
         if (!v) return

commit 81f2acf873c8bac5bd37ee49cee92bb6b13e3636

fill: data-list handling FIX (was broken in 6ee7f9e)

elu.js
@@ -512,29 +512,9 @@ function fill (jq, data, target) {
     }
 
     if (data.fake == -1) jq.attr ('data-deleted', 1)
-
-    eachAttr (jq, 'data-list',   data, function (me, n, v) {
-    
-        if (!v) {
-            console.log ('Empty value as data-list in ' + me.get(0).outerHTML, data, n)
-            me.remove ()
-            return
-        }
-    
-        if (!$.isArray (v)) {
-            console.log ('Not a list as data-list for ' + me.get(0).outerHTML + ': ', v)
-            me.remove ()
-            return
-        }
-        
-        var list = $([]); for (var i = 0; i < v.length; i ++) list = list.add (fill (me.clone (), v [i]))
-
-        me.replaceWith (list)
-        
-    })
     
     eachAttr (jq, 'data-text',   data, function (me, n, v) {
-    
+
         if (v == null) v = me.attr ('data-default') || ''
         
         var dig = me.attr ('data-digits')
@@ -654,6 +634,26 @@ function fill (jq, data, target) {
         
         })
         
+    })
+    
+    eachAttr (jq, 'data-list', data, function (me, n, v) {
+        
+            if (!v) {
+                console.log ('Empty value as data-list in ' + me.get(0).outerHTML, data, n)
+                me.remove ()
+                return
+            }
+        
+            if (!$.isArray (v)) {
+                console.log ('Not a list as data-list for ' + me.get(0).outerHTML + ': ', v)
+                me.remove ()
+                return
+            }
+            
+            var list = $([]); for (var i = 0; i < v.length; i ++) list = list.add (fill (me.clone ().removeAttr ('data-list'), v [i]))
+    
+            me.replaceWith (list)
+            
     })
     
     jq.data ('data', data)