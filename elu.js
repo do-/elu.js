do-/elu.js

commit 81f2acf873c8bac5bd37ee49cee92bb6b13e3636

fill: data-list handling FIX (was broken in 6ee7f9e)

elu.js
@@ -512,29 +512,9 @@ function fill (jq, data, target) {
     }
 
     if (data.fake == -1) jq.attr ('data-deleted', 1)
-
-    eachAttr (jq, 'data-list',   data, function (me, n, v) {
-    
-        if (!v) {
-            console.log ('Empty value as data-list in ' + me.get(0).outerHTML, data, n)
-            me.remove ()
-            return
-        }
-    
-        if (!$.isArray (v)) {
-            console.log ('Not a list as data-list for ' + me.get(0).outerHTML + ': ', v)
-            me.remove ()
-            return
-        }
-        
-        var list = $([]); for (var i = 0; i < v.length; i ++) list = list.add (fill (me.clone (), v [i]))
-
-        me.replaceWith (list)
-        
-    })
     
     eachAttr (jq, 'data-text',   data, function (me, n, v) {
-    
+
         if (v == null) v = me.attr ('data-default') || ''
         
         var dig = me.attr ('data-digits')
@@ -654,6 +634,26 @@ function fill (jq, data, target) {
         
         })
         
+    })
+    
+    eachAttr (jq, 'data-list', data, function (me, n, v) {
+        
+            if (!v) {
+                console.log ('Empty value as data-list in ' + me.get(0).outerHTML, data, n)
+                me.remove ()
+                return
+            }
+        
+            if (!$.isArray (v)) {
+                console.log ('Not a list as data-list for ' + me.get(0).outerHTML + ': ', v)
+                me.remove ()
+                return
+            }
+            
+            var list = $([]); for (var i = 0; i < v.length; i ++) list = list.add (fill (me.clone ().removeAttr ('data-list'), v [i]))
+    
+            me.replaceWith (list)
+            
     })
     
     jq.data ('data', data)

commit d678fa03c06ea3b04cec481a8295cbd6851b2041

fill: select val from data (broken in 81f2acf)

elu.js
@@ -576,9 +576,6 @@ function fill (jq, data, target) {
     
     var textInputs = 'input:text, input[type=number], input[type=range], input:password, textarea, select'
 
-    $(textInputs, jq).each (function () {$(this).val (data [this.name])})
-    $('input:radio', jq).each (function () {var me = $(this); me.prop ('checked', me.val () == data [this.name])})
-
     if (data._read_only) {    
     
         $(textInputs, jq).each (function () {
@@ -655,7 +652,10 @@ function fill (jq, data, target) {
             me.replaceWith (list)
             
     })
-    
+
+    $(textInputs, jq).each (function () {$(this).val (data [this.name])})
+    $('input:radio', jq).each (function () {var me = $(this); me.prop ('checked', me.val () == data [this.name])})
+
     jq.data ('data', data)
     
     if (target) target.empty ().append (jq)