ddf1ed58cb530b842eb3f4629c43dc512d598699
Dmitry Ovsyanko <do_@pochta.ru>
2019-05-10 12:31:48 +0300

    fill: hidden inputs

--- elu.js
+++ elu.js
@@ -697,7 +697,7 @@ function fill (jq, data, target) {
 
     clickOn ($('span.anchor', jq), onDataUriDblClick)
 
-    var textInputs = 'input:text, input[type=number], input[type=range], input:password, textarea, select'
+    var textInputs = 'input:text, input:hidden, input[type=number], input[type=range], input:password, textarea, select'
 
     if (data._can) {
         $('button[name]', jq).each (function () {
