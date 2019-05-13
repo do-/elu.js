do-/elu.js

commit ddf1ed58cb530b842eb3f4629c43dc512d598699

fill: hidden inputs

elu.js
@@ -697,7 +697,7 @@ function fill (jq, data, target) {
 
     clickOn ($('span.anchor', jq), onDataUriDblClick)
 
-    var textInputs = 'input:text, input[type=number], input[type=range], input:password, textarea, select'
+    var textInputs = 'input:text, input:hidden, input[type=number], input[type=range], input:password, textarea, select'
 
     if (data._can) {
         $('button[name]', jq).each (function () {

commit 7a6e07ef911af684fa3b201085f53e59d1d9856a

fill FIXed (was broken in ddf1ed5)

elu.js
@@ -697,7 +697,7 @@ function fill (jq, data, target) {
 
     clickOn ($('span.anchor', jq), onDataUriDblClick)
 
-    var textInputs = 'input:text, input:hidden, input[type=number], input[type=range], input:password, textarea, select'
+    var textInputs = 'input:text, input[type=hidden], input[type=number], input[type=range], input:password, textarea, select'
 
     if (data._can) {
         $('button[name]', jq).each (function () {