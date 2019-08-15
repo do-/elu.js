d610a895158c426f3d3e35c50beea8c139868c40
Dmitry Ovsyanko <do_@pochta.ru>
2019-08-15 09:53:11 +0300

    cleanup

--- elu.js
+++ elu.js
@@ -871,12 +871,6 @@ $.fn.slideAsNeeded = function (is, o) {
 
 }
 
-$.fn.showIf = function (is, o) {
-
-    if (is) this.show (o); else this.hide (o)
-
-}
-
 $.fn.saveAsXLS = function (name) {
 
     if (!name) name = $('title').text ()
